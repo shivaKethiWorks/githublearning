9f5b3ea621b3551aed4c762f2f2e54695b78b37d
sarupundu <sarupundru@gmail.com>
2025-03-17 16:40:20 -0500

    python basics

---
+++ python_class03.py
@@ -0,0 +1,28 @@
+x = [1,2,3,4]
+x. append(5)
+print(x)
+
+y=[1,3,4,5,7]
+y.insert(1,2)
+y.insert(5,6)
+print(y)
+
+z=[7,8,9,10]
+y.extend(z)
+print(z)
+
+a=["cat" , "bat"]
+a.remove("cat")
+print(a)
+
+d= {"name" : "VB" ,
+    "lan": "telugu" ,
+    "likes":10000
+    }
+
+x=d["lan"]
+print(x)
+y = d.keys()
+print(y)
+z = d.values()
+print(z)
